ae69c8d28e4f7b758fde9d01a67806c1b2332ebb
kbaba1001 <kbaba1001@gmail.com>
2014-09-27 09:19:40 +0900

    cosmetics

--- config.rb
+++ config.rb
@@ -22,7 +22,7 @@ activate :blog do |blog|
   # Matcher for blog source files
   blog.sources = 'blog/{year}-{month}-{day}-{title}.html'
   # blog.taglink = "tags/{tag}.html"
-  blog.layout = "article_layout"
+  blog.layout = 'article_layout'
   # blog.summary_separator = /(READMORE)/
   # blog.summary_length = 250
   # blog.year_link = "{year}.html"
